fa0df2a91839489e381c1410b746fea6d00677d8
Chaitanya7725 <chaitanya7725@gmail.com>
2025-04-23 20:41:24 -0400

    Pushing initial setup

---
+++ src/main/java/com/linkedin/LinkedinBatchApplication.java
@@ -0,0 +1,13 @@
+package com.linkedin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkedinBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkedinBatchApplication.class, args);
+	}
+
+}
